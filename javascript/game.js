1b26942e199d61ff386a6ca13dbf5f302aff1112
Ce Ng <ceng@Ces-MacBook-Pro.attlocal.net>
2016-05-11 11:19:16 -0500

    oh noes everything is wrong, referenced rogers doc and tried to piece my frankenstein code together

--- javascript/game.js
+++ javascript/game.js
@@ -1,16 +1,108 @@
+//Variables
 	var gottaCatchEmAll = ["psyduck", "pikachu", "bulbasaur", "squirtle", "charmander", "goldeen", "chansey", "koffing", "jigglypuff"];
+	var wordToGuess = "";
+	var randomizeWordArray = [];
+	var guessedLetters = [];
+	var blanksAndLetters = [];
+	var wins = 0;
+	var losses = 0;
+	var guessesLeft = 7;
+
+// Functions
+function loadGame() {
+	randomizeWord = gottaCatchEmAll[Math.floor(Math.random() * gottaCatchEmAll.length)];
+	randomizeWordArray = randomizeWord.split("");
+	for (var i = 0; i < randomizeWord.length; i++) {
+		blanksAndLetters.push("_");
+	}
+	document.getElementById('wordToGuess').innerHTML = blanksAndLetters.join(" ");
+}
+
+function checkLetter(letter) {
+		var inWord = false;
+		for (var j = 0; j < randomizeWord.length; j++) {
+			if (letter == randomizeWordArray[i]) {
+					inWord == true;
+			}
+		}
+				if(inWord) {
+					document.getElementById('wordToGuess').innerHTML = randomizeWordArray.join(" ");
+				}
+				else {
+					document.getElementById('guessedLetters').innerHTML = guessedLetters.join();
+					guessesLeft--;
+				}
+}
+
+//Program
+loadGame();
+
+document.onkeyup = function(event) {
+	var guessedLetters = String.fromCharCode(event.keyCode).toLowerCase();
+	checkLetter(guessedLetters);
+	var inTheWord = false;
+
+	for(var i = 0; i < randomizeWord.length; i++) {
+		if(letter == randomizeWord[i]) {
+			wordToGuess = randomizeWordArray(wordToGuess, i, letter);
+			inTheWord = true;
+		}
+	}
+	if(!inTheWord && !randomizeWordArray(letter, guessedLetters)) {
+		guessedLetters.push(letter);
+	}
+
+		var html = 
+		"<p>Press a letter key to start guessing." + 
+		"<br>" +
+		wordToGuess + "</p>"
+		"<p>Letters you have guessed: " +
+		guessedLetters +
+		"</p>" 
+		"<p>Guesses you have left: " + 
+		guessesLeft + 
+		"</p>"+
+		"<p>Pokemon you have captured: " + 
+		wins + 
+		"</p>" +
+		"<p>Pokemon you have let escape: " + 
+		losses + 
+		"</p>" 
+		;
+
+		document.querySelector("#scoreboard").innerHTML = html;
+
+}
+
+
+/*
+//Variables
+	var gottaCatchEmAll = ["psyduck", "pikachu", "bulbasaur", "squirtle", "charmander", "goldeen", "chansey", "koffing", "jigglypuff"];
+	var randomizeWordArray = [];
 	var wins = 0;
 	var losses = 0;
 	var wordToGuess = ""
 	var guessedLetters = [];
 	var guessesLeft = 7;
 
+// Functions
+function loadGame() {
+	randomizeWord = gottaCatchEmAll[Math.floor(Math.random() * gottaCatchEmAll.length)];
+	randomizeWordArray = randomizeWord.split("");
+}
 
-	var randomizeWord = gottaCatchEmAll[Math.floor(Math.random() * gottaCatchEmAll.length)];
+function checkLetter(letter) {
 		for (var i = 0; i < randomizeWord.length; i++) {
-		wordToGuess = wordToGuess + "*" + " ";
-		}
+			if (letter == randomizeWordArray[i]) {
+				if inWord == true;
+			}
+				if{
 
+				}
+
+}
+
+		wordToGuess = wordToGuess + "*" + " ";
 
 	function changeLetter(str, i, letter) {
 	return str.substr(0, i) + letter + str.substr(i + 1);
@@ -35,8 +127,12 @@ function reset() {
 	guessedLetters = [];
 }
 
+//Program
+loadGame();
+
 document.onkeyup = function(event) {
-	var letter = String.fromCharCode(event.keyCode).toLowerCase();
+	var guessedLetters = String.fromCharCode(event.keyCode).toLowerCase();
+	checkLetter(guessedLetters)
 	var inTheWord = false;
 
 	for(var i = 0; i < randomizeWord.length; i++) {
@@ -74,3 +170,4 @@ document.onkeyup = function(event) {
 //$("imageDiv").html(img src='x' />");
 
 // document.wordToGuess.join(" ")
+*/
